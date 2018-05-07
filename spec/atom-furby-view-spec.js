f2e0fe7224ab646f8a8451a5f773458830ac446f
Timothy Clem <timothy.clem@gmail.com>
2018-05-07 11:31:02 -0700

    Initial project with basic onDidStopChanging

---
+++ spec/atom-furby-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFurbyView from '../lib/atom-furby-view';
+
+describe('AtomFurbyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
