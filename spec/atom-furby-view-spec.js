09d08b6200a1a6392ac11d37a48d4e925427a097
Timothy Clem <timothy.clem@gmail.com>
2018-05-09 13:46:02 -0700

    Remove extraneous functionality and files

--- spec/atom-furby-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomFurbyView from '../lib/atom-furby-view';
-
-describe('AtomFurbyView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
